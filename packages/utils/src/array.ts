652ceaf411ea27444961db2dacdcbdba7d934167
Alert <oleh.melnechyk@gmail.com>
2020-04-02 19:37:37 +0300

    change config logger and add isEmptyArray

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,6 +1,9 @@
 export const nonEmptyArr = (x: any): boolean =>
   Array.isArray(x) && x.length > 0;
 
+export const isEmptyArray = (x: any): boolean =>
+  Array.isArray(x) && x.length === 0
+
 export function getFirstOrUndefinded<T> (arr: T[] | undefined): T | undefined {
   return arr && arr.length > 0 ? arr[0] : undefined
 }
