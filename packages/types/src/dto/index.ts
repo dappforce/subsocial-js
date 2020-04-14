5db46de2d44e7d7b5323758800b4aa96d6ed461a
Subsocial <62490051+subsocialdev@users.noreply.github.com>
2020-04-14 18:30:35 +0300

    Update packages/types/src/dto/index.ts

--- packages/types/src/dto/index.ts
+++ packages/types/src/dto/index.ts
@@ -1,6 +1,7 @@
 import { CommonStruct, Blog, Post, Comment } from '../substrate/interfaces';
 import { CommonContent, BlogContent, PostContent, CommentContent } from '../offchain'
 
+// TODO refactor this class to a type. No sense in a class anymore, such as 'struct' and 'content' are now public.
 export abstract class CommonData<S extends CommonStruct, C extends CommonContent> {
 
   public struct?: S
