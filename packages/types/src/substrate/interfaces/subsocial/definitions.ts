52742d695a303d7ea2239ca304359a3ec720f992
Alex Siman <aleksandr.siman@gmail.com>
2020-05-08 13:57:22 +0300

    Add missing value to ScoringAction enum

--- packages/types/src/substrate/interfaces/subsocial/definitions.ts
+++ packages/types/src/substrate/interfaces/subsocial/definitions.ts
@@ -121,6 +121,7 @@ export default {
         'UpvotePost',
         'DownvotePost',
         'SharePost',
+        'CreateComment',
         'UpvoteComment',
         'DownvoteComment',
         'ShareComment',
