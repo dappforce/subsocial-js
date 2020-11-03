132546501cba6eca8d9b728d636f078ada498902
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-11-03 19:22:59 +0200

    Add missed ElasticIndex type

--- packages/types/src/offchain/search.ts
+++ packages/types/src/offchain/search.ts
@@ -1,34 +1,30 @@
-export type ElasticIndexTypes = 'spaces' | 'posts' | 'profiles';
+export type ElasticIndexTypes = 'spaces' | 'posts' | 'profiles' | 'all'
 
 export const ElasticIndex = {
   profiles: 'subsocial_profiles',
   spaces: 'subsocial_spaces',
-  posts: 'subsocial_posts'
-};
+  posts: 'subsocial_posts',
+}
 
-export const AllElasticIndexes = [
-  ElasticIndex.profiles,
-  ElasticIndex.spaces,
-  ElasticIndex.posts
-];
+export const AllElasticIndexes = [ElasticIndex.profiles, ElasticIndex.spaces, ElasticIndex.posts]
 
 export const ElasticFields = {
   space: {
     name: 'name',
     handle: 'handle',
     about: 'about',
-    tags: 'tags'
+    tags: 'tags',
   },
   post: {
     title: 'title',
     body: 'body',
-    tags: 'tags'
+    tags: 'tags',
   },
   comment: {
-    body: 'body'
+    body: 'body',
   },
   profile: {
     name: 'name',
-    about: 'about'
-  }
+    about: 'about',
+  },
 }
