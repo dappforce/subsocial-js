f4134a70d9bfe903d3be118846f55dbce3bafc9a
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-11-03 20:57:31 +0200

    Add one more common type

--- packages/types/src/offchain/search.ts
+++ packages/types/src/offchain/search.ts
@@ -28,3 +28,11 @@ export const ElasticFields = {
     about: 'about',
   },
 }
+
+export type EsQueryParams = {
+  q?: string
+  limit?: number
+  offset?: number
+  indexes?: ElasticIndexTypes[]
+  tagsFilter?: string[]
+}
