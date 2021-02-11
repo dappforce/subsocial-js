c199e84a64ff2e6bbc9d9c70b81f69e50a9cc574
olehmell <oleh.melnechyk@gmail.com>
2021-02-11 19:03:19 +0200

    Add email functions to utils packages

---
+++ packages/utils/src/email.ts
@@ -0,0 +1,35 @@
+import { isEmptyStr } from "./string"
+
+export const parseEmail = (email: string) => email?.trim().toLowerCase().split('@')
+
+export const formatEmail = (email?: string) => {
+	if (!email) return ''
+
+	const [ username, domain ] = parseEmail(email)
+
+	return `${username.replace(/[\W_]/g, '')}@${domain}`
+}
+
+const validEmailProviders = [
+  'gmail.com',
+  'google.com',
+  'yahoo.com',
+  'protonmail.com',
+  'hotmail.com',
+  'outlook.com',
+  'msn.com',
+  'live.com',
+  'aol.com',
+  'yandex.com',
+  'mail.ru'
+]
+
+const validEmailSet = new Set(validEmailProviders)
+
+export const isValidEmailProvider = (email?: string) => {
+  if (!email || isEmptyStr(email)) return false
+
+  const [ , domain ] = parseEmail(email)
+
+  return validEmailSet.has(domain)
+}
