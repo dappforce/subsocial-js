aca316e6a5597ad167db18ed1c004117cb4e1512
Vladyslav Samchuk <sam4uk.v1ad@gmail.com>
2021-06-03 16:28:24 +0300

    Fix docs-theme

--- themes/docs-theme/assets/js/src/typedoc/services/Viewport.ts
+++ themes/docs-theme/assets/js/src/typedoc/services/Viewport.ts
@@ -112,19 +112,19 @@ export class Viewport extends EventTarget {
         });
 
         this.dispatchEvent(event);
-        this.hideShowToolbar();
+        // this.hideShowToolbar();
     }
 
     /**
      * Handle hiding/showing of the toolbar.
      */
-    hideShowToolbar() {
-        const isShown = this.showToolbar;
-        this.showToolbar = this.lastY >= this.scrollTop || this.scrollTop <= 0;
-        if (isShown !== this.showToolbar) {
-            this.toolbar.classList.toggle("tsd-page-toolbar--hide");
-            this.secondaryNav.classList.toggle("tsd-navigation--toolbar-hide");
-        }
-        this.lastY = this.scrollTop;
-    }
+    // hideShowToolbar() {
+    //     const isShown = this.showToolbar;
+    //     this.showToolbar = this.lastY >= this.scrollTop || this.scrollTop <= 0;
+    //     if (isShown !== this.showToolbar) {
+    //         this.toolbar.classList.toggle("tsd-page-toolbar--hide");
+    //         this.secondaryNav.classList.toggle("tsd-navigation--toolbar-hide");
+    //     }
+    //     this.lastY = this.scrollTop;
+    // }
 }
