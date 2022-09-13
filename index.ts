74d22d37e01e6d28e15867714d3104c4bb07047d
olehmell <oleh.melnechyk@gmail.com>
2022-09-13 14:54:13 +0300

    Update imports and typo

--- index.ts
+++ index.ts
@@ -1,21 +1,8 @@
 export * as API from './packages/api/src/index'
 
-export * from './packages/api/src/connections/index'
-
+export * from './packages/api/src/substrate/index'
+export * from './packages/api/src/subsocial/index'
 export * from './packages/api/src/utils/index'
-export * from './packages/api/src/utils/types'
+export * as IPFS from './packages/api/src/ipfs/index'
+export * as types from './packages/api/src/types/index'
 
-export * as DTO from './packages/types/src/dto/index'
-
-export * from './packages/types/src/offchain/index'
-export * from './packages/types/src/offchain/search'
-
-export * from './packages/types/src/substrate/interfaceRegistry'
-export * from './packages/types/src/substrate/preparedTypes'
-export * from './packages/types/src/substrate/register'
-export * from './packages/types/src/substrate/registry'
-
-export * from './packages/types/src/substrate/classes/index'
-
-export * as Interfaces from './packages/types/src/substrate/interfaces/index'
-export * from '@subsocial/types/build/substrate/utils'
