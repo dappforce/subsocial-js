1e3e47354debdd17391989a51c554975e11b860e
ical10 <mochhusnir@gmail.com>
2023-01-26 23:10:16 +0700

    Update import

--- packages/api/src/types/ipfs.ts
+++ packages/api/src/types/ipfs.ts
@@ -1,5 +1,6 @@
 import { CID } from 'ipfs-http-client'
 import { IpfsCid as RuntimeIpfsCid } from '@subsocial/definitions/interfaces'
+import { TweetPostContent } from '@subsocial/utils'
 
 export type IpfsCommonContent =
   IpfsCommentContent |
@@ -19,30 +20,6 @@ export type IpfsSpaceContent = {
   email: string
   tags: string[]
   links: string[] | NamedLink[]
-}
-
-//Ref: https://github.com/PLhery/node-twitter-api-v2/blob/master/src/types/v2/tweet.definition.v2.ts
-interface ReferencedTweetV2 {
-  type: 'retweeted' | 'quoted' | 'replied_to'
-  id: string
-}
-
-interface TweetAttachmentV2 {
-  media_keys?: string[]
-  poll_ids?: string[]
-}
-
-export type TweetPostContent = {
-  id: string
-  edit_history_tweet_ids: string[]
-  username: string
-  created_at?: string
-  author_id?: string
-  conversation_id?: string
-  in_reply_to_user_id?: string
-  referenced_tweets?: ReferencedTweetV2[]
-  attachments?: TweetAttachmentV2
-  lang?: string
 }
 
 type CommonPostContent = {
