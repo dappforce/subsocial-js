012a7d42ff046ca0a0c1b53089338d9c61359c62
Mell <oleh.melnechyk@gmail.com>
2023-01-26 18:21:19 +0200

    Update naming

--- packages/utils/src/twitter.ts
+++ packages/utils/src/twitter.ts
@@ -2,7 +2,7 @@ import { TweetPostContent } from './types/twitter'
 
 const BASE_TWITTER_URL = "https://twitter.com"
 
-const parseHashtag = (text: string) => {
+const parseHashtags = (text: string) => {
   const result = text.replace(
     /(\s#)(\w+[a-zA-Z0-9]+)/g,
     ` [#$2](${BASE_TWITTER_URL}/hashtag/$2?src=hashtag_click)`
@@ -11,7 +11,7 @@ const parseHashtag = (text: string) => {
   return result
 }
 
-const parseUsername = (text: string) => {
+const parseUsernames = (text: string) => {
   const result = text.replace(
     /(?<!\w)@([a-zA-Z0-9_]+){1,15}/g,
     `[@$1](${BASE_TWITTER_URL}/$1)`
@@ -21,15 +21,15 @@ const parseUsername = (text: string) => {
 }
 
 const parseTextToMarkdown = (text: string) => {
-  const hashtagParsed = parseHashtag(text)
-  const markdownWithLinks = parseUsername(hashtagParsed)
+  const hashtagsParsed = parseHashtags(text)
+  const markdownWithLinks = parseUsernames(hashtagsParsed)
 
   return markdownWithLinks
 }
 
 export const twitterParser = {
-  parseHashtag,
-  parseUsername,
+  parseHashtags,
+  parseUsernames,
   parseTextToMarkdown,
 }
 
