5b5bf1a621c01659b3451912ca963a8a1c01b898
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-03 19:20:02 +0700

    Use autolinker for parsing twitter text

--- packages/utils/src/twitter.ts
+++ packages/utils/src/twitter.ts
@@ -1,44 +1,22 @@
 import { TweetPostContent } from './types/twitter'
+import Autolinker from 'autolinker'
 
 const BASE_TWITTER_URL = 'https://twitter.com'
 
-const parseHashtags = (text: string) => {
-  const result = text.replace(
-    /(\s#)(\w+[a-zA-Z0-9]+)/g,
-    ` [#$2](${BASE_TWITTER_URL}/hashtag/$2?src=hashtag_click)`
-  )
+export const parseTwitterTextToMarkdown = (text: string) => {
+  const parsed = Autolinker.link(text, {
+    mention: 'twitter',
+    hashtag: 'twitter',
+    stripPrefix: false,
+    replaceFn: function (match) {
+      const href = match.getAnchorHref()
+      const text = match.getAnchorText()
 
-  return result
-}
+      return `[${text}](${href})`
+    },
+  })
 
-const parseUsernames = (text: string) => {
-  const result = text.replace(
-    /(?<!\w)@([a-zA-Z0-9_]+){1,15}/g,
-    `[@$1](${BASE_TWITTER_URL}/$1)`
-  )
-
-  return result
-}
-
-const parseLinks = (text: string) => {
-  const urlRegex =
-      /\b(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))\b/g
-  return text.replace(urlRegex, '<$1>')
-}
-
-const parseTextToMarkdown = (text: string) => {
-  const linksParsed = parseLinks(text)
-  const linksAndHashtagsParsed = parseHashtags(linksParsed)
-  const markdown = parseUsernames(linksAndHashtagsParsed)
-
-  return markdown
-}
-
-export const twitterParser = {
-  parseLinks,
-  parseHashtags,
-  parseUsernames,
-  parseTextToMarkdown,
+  return parsed
 }
 
 export const createTwitterURL = (tweet: TweetPostContent) => {
