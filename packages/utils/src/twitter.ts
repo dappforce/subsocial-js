80b309d69e7fe3e2aeaeca5ea47dce801b458d0a
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-16 23:54:47 +0700

    Add comment to utils for twitter

--- packages/utils/src/twitter.ts
+++ packages/utils/src/twitter.ts
@@ -2,6 +2,7 @@ import Autolinker from 'autolinker'
 
 const BASE_TWITTER_URL = 'https://twitter.com'
 
+/** Parse plain text to markdown with resolved twitter links (hashtag and mention) */
 export const parseTwitterTextToMarkdown = (text: string) => {
   const parsed = Autolinker.link(text, {
     mention: 'twitter',
@@ -21,6 +22,7 @@ export const parseTwitterTextToMarkdown = (text: string) => {
   return parsed
 }
 
+/** Create twitter url with given username and tweet id */
 export const createTwitterURL = (tweet: { username: string; id: string }) => {
   return `${BASE_TWITTER_URL}/${tweet.username}/status/${tweet.id}`
 }
